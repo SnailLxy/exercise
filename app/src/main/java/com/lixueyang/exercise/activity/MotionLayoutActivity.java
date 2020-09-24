a1499213cecee59fa5b645f833797c08a029b87a
lixueyang <971005636@qq.com>
2020-09-24 18:21:15 +0800

    [练习]-MotionLayout练习注意事项 功能： 1、在MotionLayout出现的控件，必须在motion_scene文件中有对应id的Constraint，否则会报错

--- app/src/main/java/com/lixueyang/exercise/activity/MotionLayoutActivity.java
+++ app/src/main/java/com/lixueyang/exercise/activity/MotionLayoutActivity.java
@@ -8,6 +8,10 @@ import android.os.Bundle;
 
 import com.lixueyang.exercise.R;
 
+/**
+ * MotionLayout练习
+ * 注：在MotionLayout出现的控件，必须在motion_scene文件中有对应id的Constraint，否则会报错
+ */
 public class MotionLayoutActivity extends AppCompatActivity {
 
   public static void startMotionLayoutActivity(Activity activity) {
